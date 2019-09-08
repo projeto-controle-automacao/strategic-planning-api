52d93c49719896780cd295cbc6999ec4d78adee4
doylesm <gbalk@inf.ufsm.br>
2019-09-08 20:20:01 -0300

    remodelando banco

--- database/migrations/2019_09_08_182014_create_table_sub_frases_has_fields_and_pillars.php
+++ database/migrations/2019_09_08_182014_create_table_sub_frases_has_fields_and_pillars.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableSubFrasesHasFields extends Migration
+class CreateTableSubFrasesHasFieldsAndPillars extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreateTableSubFrasesHasFields extends Migration
      */
     public function up()
     {
-        Schema::create('sub_frases_has_fields', function (Blueprint $table) {
+        Schema::create('sub_frases_has_fields_and_pillars', function (Blueprint $table) {
             $table->unsignedBigInteger('field_id');
             $table->unsignedBigInteger('sub_frase_id');
+            $table->unsignedBigInteger('pillar_id');
             $table->timestamps();
 
             $table->foreign('field_id')->references('id')->on('fields');
             $table->foreign('sub_frase_id')->references('id')->on('sub_frases');
+            $table->foreign('pillar_id')->references('id')->on('pillars');
         });
     }
 
@@ -30,6 +32,6 @@ class CreateTableSubFrasesHasFields extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sub_frases_has_fields');
+        Schema::dropIfExists('sub_frases_has_fields_and_pillars');
     }
 }
